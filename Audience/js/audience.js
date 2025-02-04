7efff0deabf594a2391e8f711f0210bd0abeab6f
Richard Powell <romeo.sierra.powell@gmail.com>
2025-02-04 16:48:22 +0000

    Port Changes

--- Audience/js/audience.js
+++ Audience/js/audience.js
@@ -1,7 +1,7 @@
 import QuizWebSocket from "./QuizWebSocket.js";
 import SceneManager from "./SceneManager.js";
 
-const url = "ws://localhost:8080";
+const url = "ws://16.171.173.219:8080";
 
 QuizWebSocket.get(url);
 SceneManager.get();
