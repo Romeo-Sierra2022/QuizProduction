cf7d4458c44272aa01f0ebee05ad811d00ccce16
Richard Powell <romeo.sierra.powell@gmail.com>
2025-02-07 13:20:28 +0000

    Audience Functionality completed

--- WebsocketServer/server.js
+++ WebsocketServer/server.js
@@ -1,23 +1,23 @@
 import ws, { WebSocketServer } from "ws";
+import Message from "./Message.js";
 const PORT = 8080;
-let connections = [];
 const wss = new WebSocketServer({ port: PORT }, console.log(`WebsocketServer listening on port: ${PORT}`))
-// instigate heartbeat
+heartbeat();
+let currentScene = 0;
 wss.on("connection", (ws) => {
     console.log("New Connection", ws._protocol);
-
-    ws.id = connections.length;
-    connections.push(ws);
-    ws.ping();
-    console.log(ws.id, ws.readyState);
+    ws.isAlive = true
+    let voteOptions = ["A", "B", "C", "D"]
+    ws.vote = voteOptions[Math.floor(Math.random() * 4)];
+    const msg = new Message("SET_SCENE", currentScene);
+    ws.send(JSON.stringify(msg))
     ws.on("open", (e) => {
-        console.log("Open Event", e);
+        console.log("Open Event");
     });
     ws.on("message", (e) => {
-        console.log("Message Event", e);
         let msg;
         try {
-            msg = JSON.parse(e.data)
+            msg = JSON.parse(e.toString())
         } catch(err) {
             console.error("JSON Parsing Error")
             console.log(err)
@@ -26,14 +26,30 @@ wss.on("connection", (ws) => {
         switch(ws._protocol) {
             case "AUDIENCE": {
                 console.log("Audience sent msg:", msg)
+                if(msg.command === "SET_VOTE") {
+                    ws.vote = msg.data
+                }
+
             }
             break;
             case "STUDIO": {
+
                 console.log("Studio sent msg:", msg)
+                if(msg.command === "GET_VOTE") {
+                    let voteObject = getVoteObject()
+                    let msg = new Message("SET_VOTE", -1, voteObject.A, voteObject.B, voteObject.C, voteObject.D)
+                    console.log(msg)
+                    ws.send(JSON.stringify(msg))
+                }
             }
             break;
             case "CONTROLLER": {
                 console.log("Controller sent msg:", msg)
+                if(msg.command === "SET_SCENE") {
+                    currentScene = msg.data;
+                    const ogMsg = new Message("SET_SCENE", currentScene);
+                    ws.send(JSON.stringify(ogMsg))
+                }
             }
             break;
             default: {
@@ -51,7 +67,7 @@ wss.on("connection", (ws) => {
         console.log("Error Event", e);
     });
     ws.on("pong", (e) => {
-        console.log("Pong Event", e);
+        ws.isAlive = true;
     });
 });
 wss.on("close", (e) => {
@@ -63,3 +79,45 @@ wss.on("error", (e) => {
 wss.on("wsClientError", (e) => {
     console.log(e);
 });
+function heartbeat() {
+    setInterval(()=> {
+        wss.clients.forEach(client => {
+            if(client.isAlive && client.readyState === 1) {
+                client.ping()
+            } else {
+                console.log("Client lost connection")
+            }
+        })
+    },10000)
+}
+function getVoteObject() {
+    let voteObject = {A:0, B:0, C:0, D:0}
+    let count = 0
+    wss.clients.forEach(client => {
+        console.log("Count: ", ++count)
+        if(client.isAlive) {
+            switch(client.vote) {
+                case "A": {
+                    voteObject.A++
+                }
+                break;
+                case "B": {
+                    voteObject.B++
+                }
+                break;
+                case "C": {
+                    voteObject.C++
+                }
+                break;
+                case "D": {
+                    voteObject.D++
+                }
+                break;
+                default : {
+
+                }
+            }
+        }
+    })
+    return voteObject
+}
